71e310004448b75f7edd07248a38833dbf1d5257
Rahul Joseph Fernandez <49276090+rahuljosephfernandez@users.noreply.github.com>
2022-10-23 12:08:11 -0400

    fix datatype(s)

--- EFNKDE_Code_R.R
+++ EFNKDE_Code_R.R
@@ -62,13 +62,168 @@ FindabcGivenPts <- function(x1, y1, x2, y2) {
   return(c(y2 - y1, x1 - x2, (x2-x1)*y2)+(y1-y2)*x2)
 }
 
-FindIntrsctnPtGivenLines(a1,b1,c1,a2,b2,c2) {
+FindIntrsctnPtGivenLines <- function(a1,b1,c1,a2,b2,c2) {
   return(c(b2*c1 - b1*c2)/(a2*b1 - a1*b2),
          (a1*c2 - a2*c1)/(a2*b1 - b1*b2))
 }
 
 FindIfPtInt <- function(PtA, PtB, PtX) {
-  xA <- ptA[0]
-  xB <- 
+  xA <- PtA[1]
+  xB <- PtB[1]
+  xX <- PtX[1]
+  yA <- PtA[2]
+  yB <- PtB[2]
+  yX <- PtX[2]
+  if (xA == xB) {
+    AlphaIntExt <- (yX - yA)/(yB - yA)
+  } else {
+    AlphaIntExt <- (xX - xA)/(xB - xA)
+  }
+  
+  if (AlphaIntExt > 0 & AlphaIntExt < 1) {
+    IntAns = 1
+  } else {
+    IntAns = 0
+  }
+  return(IntAns)
 }
 
+RemoveOverlapWithBoundary <- function(BArr) {
+  BArrTmp <- BArr
+  NB = length(BArr)
+  BArrTmp <- rbind(BArrTmp, BArr[1])
+  RemoveCrossTalksArr <- c()
+  
+  for (iK in 1:length(AllEdgeList)) {
+    Edgeval <- AllEdgeList[iK]
+    a = Edgeval[1]
+    b = Edgeval[2]
+    Coorda <- PointArr[a]
+    Coordb <- PointArr[b]
+    abcEdge <- FindabcGivenPts(Coorda[1], Coorda[2], Coordb[1], Coordb[2])
+    
+    for (iB in 1:NB) {
+      Coordc <- BArrTmp[iB]
+      Coordd <- BArrTmp[iB + 1]
+      abcBoundary <- FindabcGivenPts(Coordc[1], Coordc[2], Coordd[1], Coordd[2])
+      IntPt <- FindIntrsctnPtGivenLines(abcEdge[1], abcEdge[2], abcEdge[3],
+                                        abcBoundary[1], abcBoundary[2], abcBoundary[3])
+      if (FindIfPtInt(Coorda, Coordb, IntPt) == 1 & FindIfPtInt(Coordc, Coordd, IntPt) == 1) {
+        RemoveCrossTalksArr <- c(RemoveCrossTalksArr, Edgeval[1], Edgeval[2])
+        break
+      }
+    }
+  }
+  UpdatedAllEdgeList <- c()
+  for (edge in AllEdgeList) {
+    AFflag <- 1
+    for (forbedge in RemoveCrossTalksArr){
+      if (edge[1] == forbedge[1] & edge[2] == forbedge[2]){
+        AFflag <- 0
+      }
+    }
+    if (AFflag == 1){
+      UpdatedAllEdgeList <- c(UpdatedAllEdgeList,edge[1]),edge[2])
+    }
+  }
+  return(UpdatedAllEdgeList)
+}
+
+if (length(BoundaryArray) > 0) {
+  AllEdgeList1 <- RemoveOverlapWithBoundary(BoundaryArray)
+}
+
+nAllEdgeList1 <- length(AllEdgeList1)
+
+vecnorm <- function(V) {
+  
+  norm <- sqrt(sum(V*V))
+  
+  return(norm)
+  
+}
+
+
+intinfogivenedge <- function(x, y, edges, points, iedge) {
+  
+  ptx <- c(x, y)
+  
+  ipta <- edges[iedge, 1]
+  
+  iptb <- edges[iedge, 2]
+  
+  pta <- c(points[ipta, 1], points[ipta, 2])
+  
+  ptb <- c(points[iptb, 1], points[iptb, 2])
+  
+  vxma <- ptx - pta
+  
+  vxmb <- ptx - ptb
+  
+  vbma <- ptb - pta
+  
+  vxma.vbma <- sum(vxma*vbma)
+  
+  vxmb.vamb <- sum(vxmb * -1*vbma)
+  
+  footoftheperp <- pta + ((vxma.vbma/(sum(vbma*vbma))) * vbma)
+  
+  lengthofperp <- vecnorm(V = ptx - footoftheperp)
+  
+  if (vxma.vbma <= 0) {
+    
+    ya <- vecnorm(V = pta - footoftheperp)
+    
+  } else if (vxmb.vamb <= 0) {
+    
+    ya <- vecnorm(V = ptb - footoftheperp)
+    
+  } else {
+    
+    ya = -1 * vecnorm(V = ptb - footoftheperp)
+    
+  }
+  
+  yb <- ya + vecnorm(V = vbma)
+  
+  return(c(lengthofperp, ya, yb))
+  
+}
+
+partial.efnkde <- function(x, y, bw, points, edges, iedgeef) {
+  
+  intinfovals <- intinfogivenedge(x = x, y = y, points = points, edges = edges, iedge = iedgeef)
+  
+  s.val <- intinfovals[1]
+  
+  ya.val <- intinfovals[2]
+  
+  yb.val <- intinfovals[3]
+  
+  l.edgeval <- yb.val - ya.val
+  
+  exp.val <- exp(-0.5 * ((s.val/bw)^2))
+  
+  denom.val <- 2 * sqrt(2 * pi) * nrow(edges) * bw * l.edgeval
+  
+  erfs.val <- erf(yb.val/(bw * sqrt(2))) - erf(ya.val/(bw * sqrt(2)))
+  
+  ansval.ef <- exp.val * erfs.val / denom.val
+  
+  return(ansval.ef)
+  
+}
+
+efnkde <- function(x, y, bw, edges, points) {
+  
+  ans <- 0
+  
+  for (i in 1:nrow(edges)) {
+    
+    ans <- ans + partial.efnkde(x = x, y = y, bw = bw, points = points, edges = edges, iedgeef = i)
+    
+  }
+  
+  return(ans)
+  
+}
